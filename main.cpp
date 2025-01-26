9a05a9594837dbd8941e1efe69d976b2e71fb293
Misha Samoylov <mikhailsamoylov95@gmail.com>
2025-01-26 10:16:55 +0700

    Added OpenSceneGraph and render model

--- main.cpp
+++ main.cpp
@@ -267,6 +267,23 @@ int LoadFileDFFWithName(IMG* pImgLoader, DXRender* render, char *name, int model
 	return 0;
 }
 
+
+#include <osg/ShapeDrawable>
+#include <osgViewer/Viewer>
+#include <osg/Geometry>
+#include <osg/Geode>
+#include <osg/PolygonMode>
+
+//osg::Geode *osgLoadFileDFF(IMG* pImgLoader, const char* name, int modelId)
+//{
+//	
+//
+//	return root;
+//}
+
+
+
+
 inline float Distance(XMVECTOR v1, XMVECTOR v2)
 {
 	return XMVectorGetX(XMVector3Length(XMVectorSubtract(v1, v2)));
@@ -357,8 +374,327 @@ void RenderScene(DXRender *render, Camera *camera)
 	gNeedRender.clear();
 }
 
-int WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
+
+int main(int argc, char **argv)
 {
+	TCHAR imgPath[] = L"C:/Games/Grand Theft Auto Vice City/models/gta3.img";
+	TCHAR dirPath[] = L"C:/Games/Grand Theft Auto Vice City/models/gta3.dir";
+
+	IMG* imgLoader = new IMG();
+	imgLoader->Open(imgPath, dirPath);
+
+	//osg::Geode* rootq = osgLoadFileDFF(imgLoader, "test", 200);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	osg::Vec3Array* vertices = new osg::Vec3Array;
+	/*vertices->push_back(osg::Vec3(0.0f, 0.0f, 0.0f));
+	vertices->push_back(osg::Vec3(1.0f, 0.0f, 0.0f));
+	vertices->push_back(osg::Vec3(1.0f, 0.0f, 1.0f));*/
+
+	//osg::ref_ptr<osg::Vec3Array> normals = new osg::Vec3Array;
+	//normals->push_back(osg::Vec3(0.0f, -1.0f, 0.0f));
+
+	//osg::ref_ptr<osg::Vec4Array> colors = new osg::Vec4Array;
+	/*colors->push_back(osg::Vec4(1.0f, 0.0f, 0.0f, 1.0f));
+	colors->push_back(osg::Vec4(0.0f, 1.0f, 0.0f, 1.0f));
+	colors->push_back(osg::Vec4(0.0f, 0.0f, 1.0f, 1.0f));*/
+
+
+	/*quad->setVertexArray(vertices.get());*/
+
+	//quad->setNormalArray(normals.get());
+	//quad->setNormalBinding(osg::Geometry::BIND_OVERALL);
+
+	/*quad->setColorArray(colors.get());
+	quad->setColorBinding(osg::Geometry::BIND_PER_VERTEX);
+
+	quad->addPrimitiveSet(new osg::DrawArrays(GL_TRIANGLES, 0, 3));*/
+
+
+	/* Skip LOD files */
+	//if (strstr(name, "LOD") != NULL) {
+	//	return 0;
+	//}
+
+	//char result_name[MAX_LENGTH_FILENAME + 4];
+	////strcpy(result_name, name);
+	//strcat(result_name, ".dff");
+
+	//int fileId = pImgLoader->GetFileIndexByName(result_name);
+	//if (fileId == -1) {
+	//	return NULL;
+	//}
+
+	char* fileBuffer = (char*)imgLoader->GetFileById(200);
+
+	Clump* clump = new Clump();
+	clump->Read(fileBuffer);
+
+	/*Model* model = new Model();
+	model->SetId(modelId);
+	model->SetName(name);*/
+
+	int countVertices = 0;
+
+	osg::DrawElementsUInt* indices = new osg::DrawElementsUInt();
+
+	for (uint32_t index = 0; index < clump->m_numGeometries; index++) {
+
+
+		std::vector<ModelMaterial> materIndex;
+
+		/* Load all materials */
+		uint32_t materials = clump->GetGeometryList()[index]->m_numMaterials;
+		for (uint32_t i = 0; i < materials; i++) {
+			Material* material = clump->GetGeometryList()[index]->materialList[i];
+
+			struct ModelMaterial matInd;
+			std::string b = material->texture.name;
+			//matInd.materialName = b;
+			memcpy(matInd.materialName, b.c_str(), sizeof(matInd.materialName));
+			matInd.index = i;
+
+			materIndex.push_back(matInd);
+		}
+
+		/*for (uint32_t i = 0; i < clump->GetGeometryList()[index].vertexCount; i++) {
+
+			// Load texture coordinates model
+			if (clump->GetGeometryList()[index].flags & FLAGS_TEXTURED
+				// || clump->GetGeometryList()[index].flags & FLAGS_TEXTURED2
+				) {
+				for (uint32_t j = 0; j < 1 / clump->GetGeometryList()[index].numUVs /; j++) { / insert now FLAGS_TEXTURED /
+
+					float tx = clump->GetGeometryList()[index].texCoords[j][i * 2 + 0];
+					float ty = clump->GetGeometryList()[index].texCoords[j][i * 2 + 1];
+
+					modelTextureCoord.push_back(tx);
+					modelTextureCoord.push_back(ty);
+				}
+			}
+		}*/
+
+
+
+		/* Loop for every mesh */
+		for (uint32_t i = 0; i < clump->GetGeometryList()[index]->splits.size(); i++) {
+
+			int v_count = clump->GetGeometryList()[index]->vertexCount;
+
+			/* Save to data for create vertex buffer (x,y,z tx,ty) */
+			// TODO: Free memory
+			float* meshVertexData = (float*)malloc(sizeof(float) * v_count * 5);
+
+			for (int v = 0; v < v_count; v++) {
+				float x = clump->GetGeometryList()[index]->vertices[v * 3 + 0];
+				float y = clump->GetGeometryList()[index]->vertices[v * 3 + 1];
+				float z = clump->GetGeometryList()[index]->vertices[v * 3 + 2];
+
+				float tx = 0.0f;
+				float ty = 0.0f;
+				if (clump->GetGeometryList()[index]->flags & FLAGS_TEXTURED) {
+					tx = clump->GetGeometryList()[index]->texCoords[0][v * 2 + 0];
+					ty = clump->GetGeometryList()[index]->texCoords[0][v * 2 + 1];
+				}
+
+				/*
+				 * Flip coordinates. We use Left Handed Coordinates,
+				 * but GTA engine use own coordinate system:
+				 * X – east/west direction
+				 * Y – north/south direction
+				 * Z – up/down direction
+				 * @see https://gtamods.com/wiki/Map_system
+				*/
+
+				vertices->push_back(osg::Vec3(x, y, z));
+
+				countVertices += 3;
+				/*meshVertexData[v * 5 + 0] = x;
+				meshVertexData[v * 5 + 1] = z;
+				meshVertexData[v * 5 + 2] = y;*/
+
+
+
+				meshVertexData[v * 5 + 3] = tx;
+				meshVertexData[v * 5 + 4] = ty;
+			}
+
+
+			indices = new osg::DrawElementsUInt(GL_TRIANGLES, 
+				clump->GetGeometryList()[index]->splits[i].m_numIndices
+			);
+
+			//indices->addElement((unsigned int)clump->GetGeometryList()[index]->splits[i].indices);
+
+			memcpy((void*)indices->getDataPointer(), (unsigned int*)clump->GetGeometryList()[index]->splits[i].indices,
+				sizeof(unsigned int) * clump->GetGeometryList()[index]->splits[i].m_numIndices);
+
+			//(*indices) = ;
+
+			/*(*indices)[0] = 0;
+			(*indices)[0] = 0;
+			(*indices)[0] = 0;*/
+
+
+
+			/*D3D_PRIMITIVE_TOPOLOGY topology =
+				clump->GetGeometryList()[index]->faceType == FACETYPE_STRIP
+				? D3D_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP
+				: D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
+
+			Mesh* mesh = new Mesh();
+
+			mesh->Init(
+				NULL,
+				meshVertexData,
+				v_count * 5,
+				(unsigned int*)clump->GetGeometryList()[index]->splits[i].indices,
+				clump->GetGeometryList()[index]->splits[i].m_numIndices,
+				topology
+			);*/
+
+			//uint32_t materialIndex = clump->GetGeometryList()[index]->splits[i].matIndex;
+
+			//int matIndex = -1;
+
+			//// Find texture by index
+			//for (int ib = 0; ib < materIndex.size(); ib++) {
+
+			//	if (materialIndex == materIndex[ib].index) {
+
+			//		for (int im = 0; im < g_Textures.size(); im++) {
+			//			if (strcmp(g_Textures[im].name, materIndex[ib].materialName) == 0) {
+			//				matIndex = im;
+			//				break;
+			//			}
+			//		}
+
+			//	}
+			//}
+
+			//if (matIndex != -1) {
+			//	mesh->SetAlpha(g_Textures[matIndex].IsAlpha);
+
+			//	if (g_Textures[matIndex].IsAlpha) {
+			//		model->SetAlpha(true);
+			//	}
+
+			//	mesh->SetDataDDS(
+			//		render,
+			//		g_Textures[matIndex].source,
+			//		g_Textures[matIndex].size,
+			//		g_Textures[matIndex].width,
+			//		g_Textures[matIndex].height,
+			//		g_Textures[matIndex].dxtCompression,
+			//		g_Textures[matIndex].depth
+			//	);
+			//}
+
+			//model->AddMesh(mesh);
+		}
+	}
+
+	
+	
+	
+	osg::Geometry* quad = new osg::Geometry;
+	quad->setVertexArray(vertices);
+	//quad->addPrimitiveSet(new osg::DrawArrays(GL_TRIANGLES, 0, countVertices));
+
+	quad->addPrimitiveSet(indices);
+
+	//osg::ref_ptr<osg::PolygonMode> pm = new osg::PolygonMode;
+	//pm->setMode(osg::PolygonMode::FRONT_AND_BACK, osg::PolygonMode::LINE);
+	//quad->getOrCreateStateSet()->setAttribute(pm.get());
+
+	osg::Geode* rootq = new osg::Geode;
+	rootq->addDrawable(quad);
+
+	//clump->Clear();
+	//delete clump;
+
+
+
+
+
+
+
+	
+
+
+	osg::ref_ptr<osg::Vec3Array> verticesq = new osg::Vec3Array;
+	verticesq->push_back(osg::Vec3(0.0f, 0.0f, 0.0f));
+	verticesq->push_back(osg::Vec3(1.0f, 0.0f, 0.0f));
+	verticesq->push_back(osg::Vec3(1.0f, 0.0f, 1.0f));
+
+	osg::ref_ptr<osg::Vec3Array> normals = new osg::Vec3Array;
+	normals->push_back(osg::Vec3(0.0f, -1.0f, 0.0f));
+
+	osg::ref_ptr<osg::Vec4Array> colors = new osg::Vec4Array;
+	colors->push_back(osg::Vec4(1.0f, 0.0f, 0.0f, 1.0f));
+	colors->push_back(osg::Vec4(0.0f, 1.0f, 0.0f, 1.0f));
+	colors->push_back(osg::Vec4(0.0f, 0.0f, 1.0f, 1.0f));
+
+	osg::ref_ptr<osg::Geometry> quadq = new osg::Geometry;
+	quadq->setVertexArray(verticesq.get());
+
+	quad->setNormalArray(normals.get());
+	quad->setNormalBinding(osg::Geometry::BIND_OVERALL);
+
+	quadq->setColorArray(colors.get());
+	quadq->setColorBinding(osg::Geometry::BIND_PER_VERTEX);
+
+	quadq->addPrimitiveSet(new osg::DrawArrays(GL_TRIANGLES, 0, 3));
+
+
+
+	osg::ref_ptr<osg::Geode> rootqq = new osg::Geode;
+	rootqq->addDrawable(quadq.get());
+
+
+	osg::Group* root = new osg::Group;
+	root->addChild(rootq);
+	root->addChild(rootqq);
+
+	
+
+	osgViewer::Viewer viewer;
+
+	//osg::ref_ptr<osg::Camera> camera = viewer.getCamera();
+	//if (camera->getViewMatrix().isNaN())
+	//camera->setViewMatrix(osg::Matrix::identity());
+
+	viewer.setSceneData(root);
+
+	// if the view matrix is invalid (NaN), use the identity
+
+	osg::Vec3d eye(50, 50, 0.0);
+	osg::Vec3d center(0.0, 0.0, 0.0);
+	osg::Vec3d up(0.0, 0.0, 1.0);
+
+	viewer.getCamera()->setViewMatrixAsLookAt(eye, center, up);
+
+	return viewer.run();
+
+	viewer.realize();
+	while (!viewer.done()) {
+		viewer.frame();
+	}
+	
+	return 0;
+
 	bool vsync = true;
 
 	if (!DirectX::XMVerifyCPUSupport()) {
@@ -367,22 +703,22 @@ int WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPS
 	}
 
 	Window* window = new Window();
-	window->Init(hInstance, nCmdShow, WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
+	window->Init(NULL, NULL, WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
 
 	Input* input = new Input();
-	input->Init(hInstance, window->GetHandleWindow());
+	input->Init(NULL, window->GetHandleWindow());
 
-	Camera* camera = new Camera();
-	camera->Init(WINDOW_WIDTH, WINDOW_HEIGHT);
+	//Camera* camera = new Camera();
+	//camera->Init(WINDOW_WIDTH, WINDOW_HEIGHT);
 
 	DXRender* render = new DXRender();
 	render->Init(window->GetHandleWindow(), vsync);
 
-	TCHAR imgPath[] = L"C:/Games/Grand Theft Auto Vice City/models/gta3.img";
-	TCHAR dirPath[] = L"C:/Games/Grand Theft Auto Vice City/models/gta3.dir";
+	/*TCHAR imgPath[] = L"C:/Games/Grand Theft Auto Vice City/models/gta3.img";
+	TCHAR dirPath[] = L"C:/Games/Grand Theft Auto Vice City/models/gta3.dir";*/
 
-	IMG* imgLoader = new IMG();
-	imgLoader->Open(imgPath, dirPath);
+	IMG* imgLoaderq = new IMG();
+	imgLoaderq->Open(imgPath, dirPath);
 
 	char maps[][MAX_LENGTH_FILENAME] = {
 		{ "airport" },
@@ -572,9 +908,9 @@ int WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPS
 				mouseLastState = mouseCurrState;
 			}
 
-			camera->Update(camPitch, camYaw, moveLeftRight, moveBackForward);
+			//camera->Update(camPitch, camYaw, moveLeftRight, moveBackForward);
 
-			RenderScene(render, camera);
+			//RenderScene(render, camera);
 
 			moveLeftRight = 0.0f;
 			moveBackForward = 0.0f;
@@ -582,7 +918,7 @@ int WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPS
 	}
 
 	render->Cleanup();
-	camera->Cleanup();
+	//camera->Cleanup();
 	input->Cleanup();
 
 	for (int i = 0; i < g_ipl.size(); i++) {
@@ -600,7 +936,7 @@ int WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPS
 		delete g_models[i];
 	}
 
-	delete camera;
+	//delete camera;
 	delete input;
 	delete render;
 
